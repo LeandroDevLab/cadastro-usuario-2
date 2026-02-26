f27134ba27701c83b717ef5fb0b5fa59165a1015
LeandroDevLab <prof.oliota@gmail.com>
2026-02-25 22:40:39 -0300

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Title, Container } from "./styles";
+
 function App() {
   return (
     <>
-      <div>
-        <p>Iniciando o projeto no zero</p>
-      </div>
+      <Container>
+        <h1 style={{ color: "blue", fontSize: 70 }}>Olá Olá</h1>
+        <p>Teste</p>
+        <p>Teste2</p>
+        <Title>Ok React</Title>
+      </Container>
     </>
   );
 }
