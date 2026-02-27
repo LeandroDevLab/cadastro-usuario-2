bee80103f2eaf7195aff4cdba4a7efd071c0bcd0
LeandroDevLab <prof.oliota@gmail.com>
2026-02-26 23:05:02 -0300

    :sparkles: feat: connect frontend to backend API and implement POST integration

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import api from "../../services/api";
+
 import {
   Title,
   Container,
@@ -12,6 +15,21 @@ import {
 import UsersImage from "../../assets/users.png";
 
 function Home() {
+  //useRef, criar uma referência em tempo real de algum lugar, no caso um input
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function registerNewUser() {
+    const data = await api.post("/usuarios", {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value,
+    });
+    console.log(data);
+    //console.log(inputName.current.value);
+  }
+
   return (
     <>
       <Container>
@@ -26,22 +44,25 @@ function Home() {
               <InputLabel>
                 Nome<span> *</span>
               </InputLabel>
-              <Input type="text" placeholder="Nome do usuário" />
+              {/*                                 referência dentro do input */}
+              <Input type="text" placeholder="Nome do usuário" ref={inputName} />
             </div>
             <div>
               <InputLabel>
                 Idade<span> *</span>
               </InputLabel>
-              <Input type="number" placeholder="Idade do usuário" />
+              <Input type="number" placeholder="Idade do usuário" ref={inputAge} />
             </div>
           </ContainerInput>
           <div style={{ width: "100%" }}>
             <InputLabel>
               E-mail<span> *</span>
             </InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" />
+            <Input type="email" placeholder="E-mail do usuário" ref={inputEmail} />
           </div>
-          <Button>Cadastrar Usuário</Button>
+          <Button type="button" onClick={registerNewUser}>
+            Cadastrar Usuário
+          </Button>
         </Form>
       </Container>
     </>
